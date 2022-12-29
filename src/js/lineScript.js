52d0e7a231b50c22feeb323ddc355fd9564e4344
LAPTOP-24PKLD88\vandu <vanduonghcmus@gmail.com>
2022-12-29 23:06:45 +0700

    commit 2

--- src/js/lineScript.js
+++ src/js/lineScript.js
@@ -1,29 +1,63 @@
 import * as THREE from "three";
+
+const WINDOW_SIZE = 0.5;
+
+let w = 30 * WINDOW_SIZE;
+let l = 40 * WINDOW_SIZE;
+let h = 7 * WINDOW_SIZE;
+
+function drawBox(x, y, dx, dy) {
+  const points = [];
+  points.push(new THREE.Vector2(dx, dy));
+  points.push(new THREE.Vector2(dx, y));
+  points.push(new THREE.Vector2(x, y));
+  points.push(new THREE.Vector2(x, dy));
+  points.push(new THREE.Vector2(dx, dy));
+  return points;
+}
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(
+  window.innerWidth * WINDOW_SIZE,
+  window.innerHeight * WINDOW_SIZE
+);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
-  1,
-  500
+  0.1,
+  1000
 );
 camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
+camera.lookAt(0, -10, 0);
 
 const scene = new THREE.Scene();
 
 const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const points = [];
-points.push(new THREE.Vector3(0, 0, 0));
-points.push(new THREE.Vector3(20, 0, 0));
-
+const box1 = drawBox(l, h, 0, 0);
+const box2 = drawBox(l, w + h, 0, h);
+const box3 = drawBox(l, w + h * 2, 0, w + h);
+const box4 = drawBox(l, (w + h) * 2, 0, w + h * 2);
+const box5 = drawBox(-h, w + h, 0, h);
+const box6 = drawBox(l + h, h + w, l, h);
 
 const geometry = new THREE.BufferGeometry();
-geometry.setFromPoints(points);
+
+geometry.setFromPoints(box1.concat(box2, box3, box4, box5, box6));
+
+geometry.rotateX(30);
 
 const line = new THREE.Line(geometry, material);
 
+line.position.set(-20, -30, 0);
 scene.add(line);
-renderer.render(scene, camera);
+let step = 0;
+let speed = 0.01;
+function animate() {
+  geometry.rotateX = step;
+  geometry.rotateY = step;
+  step+=speed
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
---
+++ src/js/shape.js
@@ -0,0 +1,22 @@
+import * as THREE from "three";
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+  camera.position.set(0, 0, 100);
+  camera.lookAt(0, 0, 0);
+  
+  const scene = new THREE.Scene();
+  
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+renderer.render(scene, camera);
